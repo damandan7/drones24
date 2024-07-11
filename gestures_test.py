3aabb9b35d901816d18bde0b533b395733a683b6
Daniel Shaw <144159069+damandan7@users.noreply.github.com>
2024-07-11 11:20:44 -0400

    Update gestures_test.py

--- gestures_test.py
+++ gestures_test.py
@@ -1,8 +1,18 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2
-import math
+import time
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.utils import uri_helper
+from cflib.positioning.position_hl_commander import PositionHlCommander
 
-# Set and tracking stuff
+
+cflib.crtp.init_drivers()
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+
+# Setup and tracking stuff
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 hand_grabbed1 = False
@@ -15,110 +25,117 @@ pointer = False
 middle = False
 ring = False
 pinky = False
+palm = False
 
 # Get the frame width to center the hand position
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 center_x = frame_width // 2
 
-#Running and Gesture Recognition
-while True:
+
+with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+    with PositionHlCommander(scf) as pc:
     
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
-    hands, img = detector.findHands(img)
-    
-    if hands:
-        hand1 = hands[0]
-        lmList1 = hand1['lmList']
-        fingers1 = detector.fingersUp(hand1)
+        try:
 
-        if len(hands) == 2:
-                cv2.putText(img, f'Hand Grabbed', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-                hand2 = hands[1]
-                lmList2 = hand2["lmList"]
-                fingers2 = detector.fingersUp(hand2)
-
-            #Hand Grabbed 2
-                if fingers2 == [0,0,0,0,0]:
-                    hand_grabbed2 = True
-                    
-                else:
-                    if hand_grabbed2:
-                        hand_grabbed2 = False
+            while True:
+                success, img = cap.read()
+                img = cv2.flip(img, 1)
+                hands, img = detector.findHands(img)
                 
+                if hands:
+                    hand1 = hands[0]
+                    lmList1 = hand1['lmList']
+                    fingers1 = detector.fingersUp(hand1)
 
-#Thumb
-        if fingers1 == [1,0,0,0,0]:
-            thumb = True
-            cv2.putText(img, f'Thumb', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if thumb:
-                thumb = False
-#Pointer
-        if fingers1 == [0,1,0,0,0]:
-            pointer = True
-            cv2.putText(img, f'Pointer', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if pointer:
-                pointer = False
-#Middle
-        if fingers1 == [0,0,1,0,0]:
-            middle = True
-            cv2.putText(img, f'Middle', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if middle:
-                middle = False
-#Ring       
-        if fingers1 == [0,0,0,1,0]:
-            ring = True
-            cv2.putText(img, f'Ring', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if ring:
-                ring = False
-#Pinky        
-        if fingers1 == [0,0,0,0,1]:
-            pinky = True
-            cv2.putText(img, f'Pinky', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if pinky:
-                pinky = False
-#Peace
-        if fingers1 == [0,1,1,0,0]:
-            peace = True
-            cv2.putText(img, f'Peace', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if peace:
-                peace = False
-#Rock        
-        if fingers1 == [0,1,0,0,1]:
-            rock = True
-            cv2.putText(img, f'Rock', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if rock:
-                rock = False
-                
-#Call
-        if fingers1 == [1,0,0,0,1]:
-            call = True
-            cv2.putText(img, f'Call', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        else:
-            if call:
-                call = False
-#Hand Grabbed 1
-        if fingers1 == [0, 0, 0, 0, 0]:  
-            hand_grabbed1 = True
-            cv2.putText(img, f'Hand Grabbed', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-                
-        else:
-            if hand_grabbed1:
-                hand_grabbed1 = False
+            #Thumb
+                    if fingers1 == [1,0,0,0,0]:
+                        cv2.putText(img, f'Thumb', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)  
+                        if not thumb:
+                            thumb = True
+                            pc.back(0.4, 0.3)
+                    else:
+                        thumb = False
+            #Pointer
+                    if fingers1 == [0,1,0,0,0]:
+                        cv2.putText(img, f'Pointer', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                        
+                        if not pointer:
+                            pointer = True
+                            pc.forward(0.4, 0.3)
+                        
+                    else:
+                        pointer = False
+            #Middle
+                    if fingers1 == [0,0,1,0,0]:
+                        middle = True
+                        cv2.putText(img, f'Middle', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    else:
+                        if middle:
+                            middle = False
+            #Ring       
+                    if fingers1 == [0,0,0,1,0]:
+                        ring = True
+                        cv2.putText(img, f'Ring', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    else:
+                        if ring:
+                            ring = False
+            #Pinky        
+                    if fingers1 == [0,0,0,0,1]:
+                        cv2.putText(img, f'Pinky', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                        if not pinky:
+                            pinky = True
+                            pc.up(0.4, 0.3)
+                    else:
+                        pinky = False
+            #Peace
+                    if fingers1 == [0,1,1,0,0]:
+                        cv2.putText(img, f'Peace', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                        if not peace:
+                            peace = True
+                            pc.down(0.4, 0.3)
+                    else:
+                        peace = False
+            #Rock n roll       
+                    if fingers1 == [0,1,0,0,1]:
+                        rock = True
+                        cv2.putText(img, f'Rock', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    else:
+                        if rock:
+                            rock = False
+                            
+            #Call
+                    if fingers1 == [1,0,0,0,1]:
+                        call = True
+                        cv2.putText(img, f'Call', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    else:
+                        if call:
+                            call = False
+            #Hand Grabbed 1
+                    if fingers1 == [0, 0, 0, 0, 0]:  
+                        hand_grabbed1 = True
+                        cv2.putText(img, f'Hand Grabbed', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                        
+            #Palm
+                    if fingers1 == [1, 1, 1, 1, 1]:
+                        cv2.putText(img, f'Palm', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                        if not palm:
+                            palm = True
+                            pc.land(0.4, 0.1)
+                        else:
+                            palm = False
+                        
+
+                #img Display
+                cv2.imshow("Image", img)
 
 
-    #img Display
-    cv2.imshow("Image", img)
+                if cv2.waitKey(1) & 0xFF == ord('x'):
+                    break
 
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        finally:
+            print('Landing.')
+            cap.release()
+            cv2.destroyAllWindows()
+        
 cap.release()
 cv2.destroyAllWindows()
